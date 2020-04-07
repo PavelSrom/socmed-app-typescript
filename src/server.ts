8c0e5fb56a7a0e8aaf6ef0c9d29fbb1718128d1f
PavelSrom <srompavel98@gmail.com>
2020-04-07 21:06:33 +0200

    path fix

--- src/server.ts
+++ src/server.ts
@@ -31,9 +31,9 @@ mongoose
   })
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('../../client/build'))
+  app.use(express.static('../client/build'))
 
   app.get('*', (req, res) => {
-    res.sendfile(path.resolve(__dirname, '..', '..', 'client', 'build', 'index.html'))
+    res.sendfile(path.resolve(__dirname, '..', 'client', 'build', 'index.html'))
   })
 }
